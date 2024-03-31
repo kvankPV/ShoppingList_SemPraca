62da71bc6d18d575b5898063417467024a0b86ed
Quang <quang.pviet17@gmail.com>
2024-03-31 22:02:35 +0200

    zaciatky.

---
+++ app/src/main/java/com/example/shoppinglist_sempraca/ShoppingScreen.kt
@@ -0,0 +1,60 @@
+package com.example.shoppinglist_sempraca
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+enum class ShoppingScreenTab(@StringRes val title: Int) {
+    Home(title = R.string.app_name),
+    Product(title = R.string.add_product),
+    Summary(title = R.string.summary),
+    Habits(title = R.string.habits)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShoppingScreenBar(
+    currentScreen: ShoppingScreenTab,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(currentScreen.title)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun ShoppingScreen(
+    /*viewModel: OrderViewModel = viewModel(),*/
+    navController: NavHostController = rememberNavController()
+) {
+
+}
